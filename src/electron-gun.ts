fd307b14c43e96b1ca2a6154ac09038c810e04c0
Faraz Sohi <farazsohi08@gmail.com>
2023-09-04 12:57:52 +0330

    save

--- src/electron-gun.ts
+++ src/electron-gun.ts
@@ -1,4 +1,8 @@
 import JXG from 'jsxgraph';
+import { MathJax, type MathJaxObject } from 'mathjax-full/ts/components/global';
 
+const mathJax = new MathJax({})
 const board = JXG.JSXGraph.initBoard('jxgbox', { boundingbox: [-8, 8, 8, -8] });
 const p = board.create('point', [1, 3], { name: 'point' });
+
+MathJax.
