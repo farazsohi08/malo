93f8ec78bb5a2aee63eba5efba3554ebabc8e61e
Faraz Sohi <farazsohi08@gmail.com>
2023-09-10 13:45:37 +0330

    translate

--- src/diagram.vue
+++ src/diagram.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
+  <div class="flex flex-col items-center">
+    <label>
+      <div>U<sub>h</sub> = {{ coilVoltage.toFixed(2) }}V</div>
+      <input
+        v-model.number="coilVoltage"
+        min="0.0"
+        max="10.0"
+        type="range"
+        step="any"
+      />
+    </label>
+    <label>
+      <div>U<sub>a</sub> = {{ screenVoltage.toFixed(2) }}KV</div>
+      <input
+        v-model.number="screenVoltage"
+        min="0.0"
+        max="10.0"
+        type="range"
+        step="any"
+      />
+    </label>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xml:space="preserve"
@@ -13,8 +33,10 @@
         d="M710 250h-10v60h10v-60zM530 250h-10v60h10v-60z"
       />
       <path
+        class="coil"
         fill="none"
         stroke="#F60"
+        :stroke-width="0.7 + coilDependence"
         d="M500 310c2.121 0 5.438-2.298 5-5s-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5-2.878 5-5c0-2.121-5-5-5-5"
       />
       <path
@@ -23,6 +45,7 @@
         d="M350 350h170v-50h10v60H350v-10zM350 210h170v50h10v-60H350v10z"
       />
       <path
+        class="screen"
         fill="green"
         stroke="#000"
         d="M700 260h10V100h-10v160zM700 460h10V300h-10v160z"
@@ -61,10 +84,6 @@
       <path fill="none" stroke="#000" stroke-width="2" d="M280 300v125h420" />
       <path
         d="M285 300a5 5 0 1 1-10 0 5 5 0 0 1 10 0zM425 200c0 2.762-2.238 5-5 5a5 5 0 1 1 0-10 5 5 0 0 1 5 5zM705 425c0 2.763-2.238 5-5 5s-5-2.237-5-5a5 5 0 1 1 10 0z"
-      />
-      <path
-        fill="gray"
-        d="M689.875 229.513c-60.003 15-77.398 20-149.875 20v1c72.523 0 90.128-5.03 150.125-20.03l-.25-.97z"
       />
       <text
         font-family="'Arial-ItalicMT'"
@@ -108,59 +127,43 @@
       >
         A
       </text>
-      <g fill="gray">
+      <g class="field-lines" stroke="gray" stroke-width=".4" fill="gray">
+        <path
+          fill="gray"
+          d="M689.875 229.513c-60.003 15-77.398 20-149.875 20v1c72.523 0 90.128-5.03 150.125-20.03l-.25-.97z"
+        />
         <path
           d="M689.875 254.513c-59.965 14.99-107.398 20-179.875 20v1c72.523 0 120.09-5.021 180.125-20.03l-.25-.97z"
         />
-        <path stroke="gray" stroke-width=".4" d="m514 275 2-2-7 2 7 2-2-2z" />
+        <path d="m514 275 2-2-7 2 7 2-2-2z" />
         <path
           d="M510 284.513v1c72.477 0 119.91 4.979 179.875 19.97l.25-.97c-60.035-15.01-107.602-20-180.125-20z"
         />
-        <path
-          stroke="gray"
-          stroke-width=".4"
-          d="m514 285 2-2-7 2 7 2-2-2zM544 250l2-2-7 2 7 2-2-2z"
-        />
+        <path d="m514 285 2-2-7 2 7 2-2-2zM544 250l2-2-7 2 7 2-2-2z" />
         <path
           d="M540 309.513v1c72.477 0 89.872 4.97 149.875 19.97l.25-.97c-59.997-15-77.602-20-150.125-20z"
         />
-        <path stroke="gray" stroke-width=".4" d="m544 310 2-2-7 2 7 2-2-2z" />
+        <path d="m544 310 2-2-7 2 7 2-2-2z" />
         <path
           d="M540 334.513v1c72.477 0 89.872 4.97 149.875 19.97l.25-.97c-59.997-15-77.602-20-150.125-20z"
         />
-        <path stroke="gray" stroke-width=".4" d="m544 335 2-2-7 2 7 2-2-2z" />
+        <path d="m544 335 2-2-7 2 7 2-2-2z" />
         <path
           d="M689.875 204.513c-60.003 15-77.398 20-149.875 20v1c72.523 0 90.128-5.03 150.125-20.03l-.25-.97z"
         />
-        <path stroke="gray" stroke-width=".4" d="m544 225 2-2-7 2 7 2-2-2z" />
+        <path d="m544 225 2-2-7 2 7 2-2-2z" />
         <path d="M507.5 257.013v1h10v-1h-10z" />
-        <path
-          stroke="gray"
-          stroke-width=".4"
-          d="m513.5 257.5-2 2 7-2-7-2 2 2z"
-        />
+        <path d="m513.5 257.5-2 2 7-2-7-2 2 2z" />
         <path
           d="M524.656 262.138c-2.472 2.472-4.251 3.649-6.656 4.25-2.405.603-5.5.625-10.5.625v1c5 0 8.155-.007 10.75-.655s4.564-1.971 7.094-4.5l-.688-.72z"
         />
-        <path
-          stroke="gray"
-          stroke-width=".4"
-          d="M522.172 265.329v2.828l3.535-6.364-6.364 3.536h2.829z"
-        />
+        <path d="M522.172 265.329v2.828l3.535-6.364-6.364 3.536h2.829z" />
         <path d="M507.5 302.013v1h10v-1h-10z" />
-        <path
-          stroke="gray"
-          stroke-width=".4"
-          d="m513.5 302.5-2 2 7-2-7-2 2 2z"
-        />
+        <path d="m513.5 302.5-2 2 7-2-7-2 2 2z" />
         <path
           d="M507.5 292.013v1c5 0 8.095-.007 10.5.595s4.186 1.779 6.656 4.25l.688-.72c-2.528-2.528-4.499-3.851-7.094-4.5-2.595-.65-5.75-.625-10.75-.625z"
         />
-        <path
-          stroke="gray"
-          stroke-width=".4"
-          d="M522.172 294.672h-2.829l6.364 3.535-3.535-6.363v2.828z"
-        />
+        <path d="M522.172 294.672h-2.829l6.364 3.535-3.535-6.363v2.828z" />
       </g>
       <path
         fill="none"
@@ -179,63 +182,67 @@
           d="m664 280-3 3 10.5-3-10.5-3 3 3z"
         />
       </g>
-      <text
-        fill="#00F"
-        font-family="'Arial-ItalicMT'"
-        font-size="24"
-        transform="translate(648.12 269.466)"
-      >
-        V
-      </text>
       <path fill="none" stroke="#00F" d="M650 250h14l-5-5" />
-      <text
-        fill="gray"
-        font-family="'Arial-ItalicMT'"
-        font-size="56"
-        transform="translate(590 210)"
-      >
-        E
-      </text>
+      <path
+        fill="none"
+        stroke="#000"
+        d="m455 235 35 25M510 135l-20 60M712 120h40"
+      />
       <path
         fill="none"
         stroke="gray"
         stroke-width="2"
         d="M592.5 167.5h29L614 160"
       />
-      <text
-        font-family="'MyriadPro-Regular'"
-        font-size="40"
-        transform="translate(417.5 250)"
-      >
-        1
-      </text>
-      <text
-        font-family="'MyriadPro-Regular'"
-        font-size="40"
-        transform="translate(500 130)"
-      >
-        2
-      </text>
-      <text
-        font-family="'MyriadPro-Regular'"
-        font-size="40"
-        transform="translate(760 135)"
-      >
-        3
-      </text>
-      <path
-        fill="none"
-        stroke="#000"
-        d="m455 235 35 25M510 135l-20 60M712 120h40"
-      />
-      <text
-        fill="gray"
-        font-family="'ArialMT'"
-        font-size="36"
-        transform="translate(625 220)"
-      >
-        A
-      </text>
+      <g class="text" transform="translate(-2, 1.5)">
+        <text
+          fill="#00F"
+          font-family="'Arial-ItalicMT'"
+          font-size="24"
+          transform="translate(650 270)"
+        >
+          V
+        </text>
+        <text
+          fill="gray"
+          font-family="'Arial-ItalicMT'"
+          font-size="56"
+          transform="translate(590 210)"
+        >
+          E
+        </text>
+
+        <text
+          font-family="'MyriadPro-Regular'"
+          font-size="40"
+          transform="translate(417.5 250)"
+        >
+          1
+        </text>
+        <text
+          font-family="'MyriadPro-Regular'"
+          font-size="40"
+          transform="translate(500 130)"
+        >
+          2
+        </text>
+        <text
+          font-family="'MyriadPro-Regular'"
+          font-size="40"
+          transform="translate(760 135)"
+        >
+          3
+        </text>
+
+        <text
+          fill="gray"
+          font-family="'ArialMT'"
+          font-size="36"
+          transform="translate(625 220)"
+        >
+          A
+        </text>
+      </g>
       <path
         fill="none"
         stroke="#000"
@@ -294,6 +301,43 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { clamp } from 'remeda';
+import { computed, ref } from 'vue';
 
-<style scoped></style>
+const coilVoltage = ref(5);
+const screenVoltage = ref(5);
+
+const coilDependence = computed(() => {
+  const v = clamp(coilVoltage.value, { min: 0, max: 10 });
+  if (v < 2.5) return 0;
+
+  return (1 / 7.5 ** 2) * (v - 2.5) ** 2;
+});
+
+const screenDependence = computed(() => {
+  const v = clamp(screenVoltage.value, { min: 0, max: 10 });
+  if (v < 1) return 0;
+  if (v >= 3) return 1;
+
+  return 0.5 * v - 1;
+});
+
+const beamIntensity = computed(
+  () => coilDependence.value * screenDependence.value,
+);
+</script>
+
+<style scoped>
+.coil {
+  filter: drop-shadow(0 0 v-bind(coilDependence) rgb(255, 187, 0));
+}
+
+.field-lines {
+  & > path {
+    stroke-width: v-bind(0.6 + beamIntensity);
+    filter: drop-shadow(0 0 v-bind(0.6 + beamIntensity) rgb(129, 2, 123));
+    text-shadow: 0 0 3px rgb(129, 2, 123);
+  }
+}
+</style>
