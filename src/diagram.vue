c77cd2a092cd09e960e8971cc3e30daf74b11158
Faraz Saadati <fsaadatisohi@divergent3d.com>
2023-09-18 00:26:34 +0330

    beam

--- src/diagram.vue
+++ src/diagram.vue
@@ -1,23 +1,54 @@
 <template>
   <div class="flex flex-col items-center">
-    <label>
-      <div>U<sub>h</sub> = {{ coilVoltage.toFixed(2) }}V</div>
+    <label class="w-72">
+      <div class="flex justify-between">
+        <div>Cathode Voltage (U<sub>h</sub>)</div>
+        <div class="flex w-20 justify-between">
+          <div class="px-2">=</div>
+          <div>{{ cathodeVoltage.toFixed(2) }}V</div>
+        </div>
+      </div>
       <input
-        v-model.number="coilVoltage"
+        v-model.number="cathodeVoltage"
+        class="w-72"
         min="0.0"
         max="10.0"
         type="range"
-        step="any"
+        step="0.01"
       />
     </label>
-    <label>
-      <div>U<sub>a</sub> = {{ screenVoltage.toFixed(2) }}KV</div>
+    <label class="w-72">
+      <div class="flex justify-between">
+        <div>Anode Voltage (U<sub>a</sub>)</div>
+        <div class="flex w-20 justify-between">
+          <div class="px-2">=</div>
+          <div>{{ anodeVoltage.toFixed(2) }}KV</div>
+        </div>
+      </div>
       <input
-        v-model.number="screenVoltage"
+        v-model.number="anodeVoltage"
+        class="w-72"
         min="0.0"
         max="10.0"
         type="range"
-        step="any"
+        step="0.01"
+      />
+    </label>
+    <label class="w-72">
+      <div class="flex justify-between">
+        <div>Cylinder Voltage (U<sub>w</sub>)</div>
+        <div class="flex w-20 justify-between">
+          <div class="px-2">=</div>
+          <div>{{ cylinderVoltage.toFixed(2) }}KV</div>
+        </div>
+      </div>
+      <input
+        v-model.number="cylinderVoltage"
+        class="w-72"
+        min="0.0"
+        max="10.0"
+        type="range"
+        step="0.01"
       />
     </label>
     <svg
@@ -33,10 +64,16 @@
         d="M710 250h-10v60h10v-60zM530 250h-10v60h10v-60z"
       />
       <path
+        class="beam fill-lime-500"
+        :d="`M507,275 v10 ${beamSpreadPath}z`"
+        stroke="0"
+        :fill-opacity="beamIntensity"
+      />
+      <path
         class="coil"
         fill="none"
         stroke="#F60"
-        :stroke-width="0.7 + coilDependence"
+        :stroke-width="0.7 + cathodeDependence"
         d="M500 310c2.121 0 5.438-2.298 5-5s-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5.438-2.298 5-5-8.399-7.221-10-5c-1.601 2.222 2.879 5 5 5s5-2.878 5-5c0-2.121-5-5-5-5"
       />
       <path
@@ -85,43 +122,46 @@
       <path
         d="M285 300a5 5 0 1 1-10 0 5 5 0 0 1 10 0zM425 200c0 2.762-2.238 5-5 5a5 5 0 1 1 0-10 5 5 0 0 1 5 5zM705 425c0 2.763-2.238 5-5 5s-5-2.237-5-5a5 5 0 1 1 10 0z"
       />
-      <g class="field-lines" stroke="gray" stroke-width=".4" fill="gray">
-        <path
-          fill="gray"
-          d="M689.875 229.513c-60.003 15-77.398 20-149.875 20v1c72.523 0 90.128-5.03 150.125-20.03l-.25-.97z"
-        />
-        <path
-          d="M689.875 254.513c-59.965 14.99-107.398 20-179.875 20v1c72.523 0 120.09-5.021 180.125-20.03l-.25-.97z"
-        />
-        <path d="m514 275 2-2-7 2 7 2-2-2z" />
-        <path
-          d="M510 284.513v1c72.477 0 119.91 4.979 179.875 19.97l.25-.97c-60.035-15.01-107.602-20-180.125-20z"
-        />
-        <path d="m514 285 2-2-7 2 7 2-2-2zM544 250l2-2-7 2 7 2-2-2z" />
-        <path
-          d="M540 309.513v1c72.477 0 89.872 4.97 149.875 19.97l.25-.97c-59.997-15-77.602-20-150.125-20z"
-        />
-        <path d="m544 310 2-2-7 2 7 2-2-2z" />
-        <path
-          d="M540 334.513v1c72.477 0 89.872 4.97 149.875 19.97l.25-.97c-59.997-15-77.602-20-150.125-20z"
-        />
-        <path d="m544 335 2-2-7 2 7 2-2-2z" />
-        <path
-          d="M689.875 204.513c-60.003 15-77.398 20-149.875 20v1c72.523 0 90.128-5.03 150.125-20.03l-.25-.97z"
-        />
-        <path d="m544 225 2-2-7 2 7 2-2-2z" />
-        <path d="M507.5 257.013v1h10v-1h-10z" />
-        <path d="m513.5 257.5-2 2 7-2-7-2 2 2z" />
-        <path
-          d="M524.656 262.138c-2.472 2.472-4.251 3.649-6.656 4.25-2.405.603-5.5.625-10.5.625v1c5 0 8.155-.007 10.75-.655s4.564-1.971 7.094-4.5l-.688-.72z"
-        />
-        <path d="M522.172 265.329v2.828l3.535-6.364-6.364 3.536h2.829z" />
-        <path d="M507.5 302.013v1h10v-1h-10z" />
-        <path d="m513.5 302.5-2 2 7-2-7-2 2 2z" />
-        <path
-          d="M507.5 292.013v1c5 0 8.095-.007 10.5.595s4.186 1.779 6.656 4.25l.688-.72c-2.528-2.528-4.499-3.851-7.094-4.5-2.595-.65-5.75-.625-10.75-.625z"
-        />
-        <path d="M522.172 294.672h-2.829l6.364 3.535-3.535-6.363v2.828z" />
+      <g class="field-lines" stroke-width=".4">
+        <g class="anode-field-lines fill-green-500 stroke-green-500">
+          <path
+            d="M689.875 229.513c-60.003 15-77.398 20-149.875 20v1c72.523 0 90.128-5.03 150.125-20.03l-.25-.97z"
+          />
+          <path
+            d="M689.875 254.513c-59.965 14.99-107.398 20-179.875 20v1c72.523 0 120.09-5.021 180.125-20.03l-.25-.97z"
+          />
+          <path d="m514 275 2-2-7 2 7 2-2-2z" />
+          <path
+            d="M510 284.513v1c72.477 0 119.91 4.979 179.875 19.97l.25-.97c-60.035-15.01-107.602-20-180.125-20z"
+          />
+          <path d="m514 285 2-2-7 2 7 2-2-2zM544 250l2-2-7 2 7 2-2-2z" />
+          <path
+            d="M540 309.513v1c72.477 0 89.872 4.97 149.875 19.97l.25-.97c-59.997-15-77.602-20-150.125-20z"
+          />
+          <path d="m544 310 2-2-7 2 7 2-2-2z" />
+          <path
+            d="M540 334.513v1c72.477 0 89.872 4.97 149.875 19.97l.25-.97c-59.997-15-77.602-20-150.125-20z"
+          />
+          <path d="m544 335 2-2-7 2 7 2-2-2z" />
+          <path
+            d="M689.875 204.513c-60.003 15-77.398 20-149.875 20v1c72.523 0 90.128-5.03 150.125-20.03l-.25-.97z"
+          />
+          <path d="m544 225 2-2-7 2 7 2-2-2z" />
+        </g>
+        <g class="cylinder-field-lines fill-yellow-500 stroke-yellow-500">
+          <path d="M507.5 257.013v1h10v-1h-10z" />
+          <path d="m513.5 257.5-2 2 7-2-7-2 2 2z" />
+          <path
+            d="M524.656 262.138c-2.472 2.472-4.251 3.649-6.656 4.25-2.405.603-5.5.625-10.5.625v1c5 0 8.155-.007 10.75-.655s4.564-1.971 7.094-4.5l-.688-.72z"
+          />
+          <path d="M522.172 265.329v2.828l3.535-6.364-6.364 3.536h2.829z" />
+          <path d="M507.5 302.013v1h10v-1h-10z" />
+          <path d="m513.5 302.5-2 2 7-2-7-2 2 2z" />
+          <path
+            d="M507.5 292.013v1c5 0 8.095-.007 10.5.595s4.186 1.779 6.656 4.25l.688-.72c-2.528-2.528-4.499-3.851-7.094-4.5-2.595-.65-5.75-.625-10.75-.625z"
+          />
+          <path d="M522.172 294.672h-2.829l6.364 3.535-3.535-6.363v2.828z" />
+        </g>
       </g>
       <path
         fill="none"
@@ -224,54 +264,6 @@
         stroke-miterlimit="10"
         stroke-width="1.674"
       />
-      <defs>
-        <filter
-          id="drop-shadow"
-          color-interpolation-filters="sRGB"
-          x="-50%"
-          y="0%"
-          height="200%"
-          width="200%"
-        >
-          <!-- Take source alpha, offset it by angle/distance and blur it by size -->
-          <feOffset
-            id="offset"
-            in="SourceAlpha"
-            dx="5"
-            dy="0"
-            result="SA-offset"
-          />
-          <feGaussianBlur
-            id="blur"
-            in="SA-offset"
-            stdDeviation="3.75"
-            result="SA-o-blur"
-          />
-
-          <!-- Adjust the spread by multiplying alpha by a constant factor -->
-          <feComponentTransfer in="SA-o-blur" result="SA-o-b-c-sprd">
-            <feFuncA
-              id="spread-ctrl"
-              type="linear"
-              :slope="3 * beamIntensity"
-            />
-          </feComponentTransfer>
-
-          <!-- Adjust color and opacity by adding fixed offsets and an opacity multiplier -->
-          <feColorMatrix
-            id="recolor"
-            in="SA-o-b-c-sprd"
-            type="matrix"
-            values="0 0 0 0 0.404 0 0 0 0 0.976 0 0 0 0 0.243 0 0 0 0.8 0"
-            result="SA-o-b-c-s-recolor"
-          />
-          <!-- Merge the shadow with the original -->
-          <feMerge>
-            <feMergeNode in="SA-o-b-c-s-recolor" />
-            <feMergeNode in="SourceGraphic" />
-          </feMerge>
-        </filter>
-      </defs>
     </svg>
   </div>
 </template>
@@ -280,29 +272,57 @@
 import { computed, ref } from 'vue';
 import * as equations from './equations';
 
-const coilVoltage = ref(7);
-const screenVoltage = ref(3);
-const coilDependence = computed(() => equations.coil(coilVoltage.value));
-const screenDependence = computed(() => equations.screen(screenVoltage.value));
+const cathodeVoltage = ref(7);
+const anodeVoltage = ref(3);
+const cylinderVoltage = ref(0);
+const cathodeDependence = computed(() =>
+  equations.cathode(cathodeVoltage.value),
+);
+const beamSpreadPath = computed(() => {
+  const spread = cylinderVoltage.value;
+  return `l300, ${spread} v-${2 * spread + 10}`;
+});
+const anodeDependence = computed(() => equations.anode(anodeVoltage.value));
+const anodeRelativeFieldStrength = computed(() => anodeVoltage.value / 10);
+const cylinderRelativeFieldStrength = computed(
+  () => cylinderVoltage.value / 10,
+);
 const beamIntensity = computed(
-  () => coilDependence.value * screenDependence.value,
+  () => cathodeDependence.value * anodeDependence.value,
 );
 </script>
 
 <style scoped>
 .coil {
-  filter: drop-shadow(0 0 v-bind(coilDependence) rgb(255, 187, 0));
+  filter: drop-shadow(
+    0 0 v-bind(cathodeDependence + 'px') theme('colors.amber.400')
+  );
 }
 
-.field-lines {
-  & > path {
-    stroke-width: v-bind(0.6 + beamIntensity);
-    filter: drop-shadow(0 0 v-bind(0.6 + beamIntensity) rgb(129, 2, 123));
-    text-shadow: 0 0 3px rgb(129, 2, 123);
-  }
+.anode-field-lines {
+  stroke-width: v-bind(anodeRelativeFieldStrength);
+  filter: drop-shadow(
+    0 0 v-bind(2.5 * anodeRelativeFieldStrength + 'px')
+      theme('colors.green.500' / v-bind(anodeRelativeFieldStrength))
+  );
+}
+
+.cylinder-field-lines {
+  stroke-width: v-bind(cylinderRelativeFieldStrength);
+  filter: drop-shadow(
+    0 0 v-bind(2.5 * cylinderRelativeFieldStrength + 'px')
+      theme('colors.yellow.500' / v-bind(cylinderRelativeFieldStrength))
+  );
 }
 
 .screen {
-  filter: url(#drop-shadow);
+  filter: drop-shadow(
+    v-bind(5 * beamIntensity + 'px') 0 v-bind(5 * beamIntensity + 'px')
+      theme('colors.lime.500' / v-bind(beamIntensity))
+  );
+}
+
+.beam {
+  fill: linear-gradient(0 green 0.5 green 0.5 black 1 black);
 }
 </style>
