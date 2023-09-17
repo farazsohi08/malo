2dc92df1df6c359005e2b7a87b99cbcb828454e0
Faraz Saadati <fsaadatisohi@divergent3d.com>
2023-09-18 00:26:34 +0330

    beam
    s
    
    s
    
    s

--- src/equations.ts
+++ src/equations.ts
@@ -1,23 +1,27 @@
 import { clamp } from 'remeda';
 
-export function coil(coilVoltage: number): number {
-  const v = clamp(coilVoltage, { min: 0, max: 10 });
+export function cathode(voltage: number): number {
+  const v = clamp(voltage, { min: 0, max: 10 });
   if (v < 2.5) return 0;
 
   return (1 / 7.5 ** 2) * (v - 2.5) ** 2;
 }
 
-export function screen(screenVoltage: number): number {
-  const v = clamp(screenVoltage, { min: 0, max: 10 });
+export function anode(voltage: number): number {
+  const v = clamp(voltage, { min: 0, max: 10 });
   if (v < 1) return 0;
   if (v >= 3) return 1;
 
-  return 0.5 * v - 1;
+  return 0.5 * (v - 1);
+}
+
+export function beamField(voltage: number): number {
+  return (2 / 100) * voltage ** 2;
 }
 
 export function beamIntensity(
-  coilVoltage: number,
-  screenVoltage: number,
+  cathodeVoltage: number,
+  anodeVoltage: number,
 ): number {
-  return coil(coilVoltage) * screen(screenVoltage);
+  return cathode(cathodeVoltage) * anode(anodeVoltage);
 }
